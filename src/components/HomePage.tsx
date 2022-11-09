5d0cdab45dc5427a1cd8f53a9182862f5fd76d12
Hanna Nordhus <hannagn@stud.ntnu.no>
2022-11-09 19:48:30 +0100

    Edited text and colours

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,17 +3,17 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 export const HomePage = () => {
   return (
-    <div id="home" className="w-full h-screen bg-[#edede9]">
+    <div id="home" className="w-full h-screen bg-[#FEFAE0]">
       <div className="max-w-[1000 px] mx-auto px-20 flex flex-col justify-center h-full">
         <p className="text-black">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-gray-700">
-          Ada Lovelace
+          Hanna Gjone Nordhus
         </h1>
         <h2 className="text-3xl sm:text-6xl font-bold text-[#848794]">
-          I´m a developer.
+          I´m a system developer.
         </h2>
         <p className="text-[#848794] py-4 max-w-[700px]">
-          Thanks for visiting my page, feel free to check out my portifolio
+          Thanks for visiting my page, feel free to check out my portfolio
         </p>
         <div>
           <button className="text-gray-700 group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#989cab] hover:border-[#989cab] hover:text-white">
